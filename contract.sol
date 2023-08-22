be3d1c6d8e67612c92965b203c5f12ac5fee609e
Ashish Kumar <97027791+Ashish020202@users.noreply.github.com>
2023-08-22 20:39:56 +0530

    contract.sol

---
+++ contract.sol
@@ -0,0 +1,4 @@
+Declare version 
+Version Pragma
+All solidity source code should start with a "version pragma" — a declaration of the version of the Solidity compiler this code should use 
+syntax:  pragma solidity >=0.5.0 <0.6.0;
