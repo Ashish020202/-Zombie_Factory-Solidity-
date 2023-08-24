d0a52df12ad94c95c74e8a72105a9f7ca610376d
Ashish Kumar <97027791+Ashish020202@users.noreply.github.com>
2023-08-24 20:45:31 +0530

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -2,3 +2,37 @@ Declare version
 Version Pragma
 All solidity source code should start with a "version pragma" — a declaration of the version of the Solidity compiler this code should use 
 syntax:  pragma solidity >=0.5.0 <0.6.0;
+
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    // declare our event here
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // and fire it here
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
